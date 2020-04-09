fbfb9c9934ac13be0b477aec9d296b6bdb3c6f90
tfluker19 <tfluker19@DESKTOP-1LI18VI>
2020-04-09 17:33:04 -0500

    item page started

---
+++ Uplift/wwwroot/js/script.js
@@ -0,0 +1,9 @@
+﻿function swapPic1() {
+    $('#si-main-image').attr('src', $('#p1').attr('src'));
+}
+function swapPic2() {
+    $('#si-main-image').attr('src', $('#p2').attr('src'));
+}
+function swapPic3() {
+    $('#si-main-image').attr('src', $('#p3').attr('src'));
+}
